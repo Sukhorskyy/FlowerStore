026107ba2de535a9254bc01f60080fb46ae31532
Sukhorskyy <yurii.sukhorskyi@ucu.edu.ua>
2021-11-07 14:37:00 +0200

    Improved new functionality

--- src/main/java/Order/QuickOrder.java
+++ src/main/java/Order/QuickOrder.java
@@ -2,9 +2,4 @@ package Order;
 
 public enum QuickOrder {
     ROSE_BUCKET, TULIP_BUCKET, CHAMOMILE_BUCKET;
-
-    public Order createOrder(QuickOrder quickOrder) {
-        Order order = new Order();
-        return order;
-    }
 }
